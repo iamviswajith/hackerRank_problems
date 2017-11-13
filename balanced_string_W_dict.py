c02da0ba9f791da1a7096a416588d733c4012052
iamviswajith <nydhruva.viswajith@gmail.com>
2017-11-13 10:49:45 -0800

    different solutions for balanced brackets

--- balanced_string_W_dict.py
+++ balanced_string_W_dict.py
@@ -1,18 +1,16 @@
 def is_matched(expression):
-
     start_time = timeit.default_timer()
+    dic = {"{":"}",
+           "[":"]",
+           "(":")"}
     stack = []
     for char in expression:
-       if char is "{":
-           stack.append("}")
-       elif char is "[":
-           stack.append("]")
-       elif char is "(":
-           stack.append(")")
-       else:
-           if len(stack) is 0 or char != stack[-1]:
-               return False
-           stack.pop()
+        if char in dic:
+            stack.append(dic[char])
+        else:
+            if len(stack) == 0 or char != stack[-1]:
+                return False
+            stack.pop()
     elapsed = timeit.default_timer() - start_time
     print (elapsed)
     return len(stack) == 0
